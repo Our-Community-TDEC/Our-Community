f7ba24cebc56f633ef70b9d5c90b3b8e090d666a
AkshDesai04 <dev.akshdesai@gmail.com>
2022-07-24 03:05:34 +0530

    Added (and then commented coz beta) some code that might eventually work

--- lib/screens/BetaTestSection/PiCharts.dart
+++ lib/screens/BetaTestSection/PiCharts.dart
@@ -1,28 +1,24 @@
-import 'dart:math' as math;
-import 'dart:io';
-
 import 'package:final_year_project/logic/voting_logic.dart';
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
-// import 'package:fl_chart/fl_chart.dart';
 
 class Pie_Chart_Widget extends StatelessWidget with voting_logic {
 
-  late int person_1_votes = 2, person_2_votes = 3;
+  late int person_1_votes = 5, person_2_votes = 5;
   late String person_1_name = "Aksh", person_2_name = "College";
 
-  Pie_Chart_Widget() {
-    person_1_votes = 2;
-    person_2_votes = 3;
-    person_1_name = "Aksh";
-    person_2_name = "College";
-  }
-
-  Map<String, double> dataMap = {
-    // "person_1_name": person_1_votes,
+  Map<String, double> datamap = {
     "person_1_name": 1,
     "person_2_name": 2,
   };
+
+  // late Map<String, double> dataMap = {};
+  // dataMap[person_1_name] = person_1_votes;
+
+  // Map datamap = new Map<String, double>.fromIterable(letters, numbers);
+  // Map datamap = new Map();
+  // datamap.putIfAbsent("Aksh", () => 1);
+
 
   List<Color> colorList = [
     Color(0xff000000),
@@ -39,7 +35,7 @@ class Pie_Chart_Widget extends StatelessWidget with voting_logic {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 PieChart(
-                  dataMap: dataMap,
+                  dataMap: datamap,
                   colorList: colorList,
                   chartRadius: MediaQuery.of(context).size.width / 2,
                   centerText: "Budget",
