0aa7ae9be1e0d21505f934345b2d7a69b4160d2e
AkshDesai04 <dev.akshdesai@gmail.com>
2022-08-01 19:11:22 +0530

    Added validity testing to loginPage

--- lib/logic/login_logic.dart
+++ lib/logic/login_logic.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: avoid_print
+
 class Login_Logic {
   void signInButtonPressed(String email, String pass) {
     print("MailID: $email");
     print("Passwd: $pass");
 
-    if(emailValidityTest(email))
+    if (emailValidityTest(email))
       print("Ok");
     else
       print("There is an issue");
@@ -11,20 +13,26 @@ class Login_Logic {
     print(email.indexOf('@'));
     print(email.lastIndexOf('.'));
 
-    if(email == "aksh.d.4002@gmail.com" && pass == "Aksh1234;") {
-      print("Username and Password ok\nTaking you to next page...");
-    }
-    else {
+    if (verify("aksh.d.4002@gmail.com", "Aksh1234;")) {
+      print("Username and Password ok\nTaking you to next page...\nNew");
+    } else {
       print("Something wrong.\nPlease try again.");
     }
+
+    print(verify(email, pass));
   }
 
   bool emailValidityTest(String email) {
-    if(email.indexOf(' ') == -1)
-      if(email.indexOf('@') != -1)
-        if(email.lastIndexOf('.') > email.indexOf('@'))
-          return true;
+    if (email.indexOf(' ') == -1) if (email.indexOf('@') !=
+        -1) if (email.lastIndexOf('.') > email.indexOf('@')) return true;
 
+    return false;
+  }
+
+  bool verify(String email, String pass) {
+    print("I got email as: $email");
+    print("I got passw as: $pass ");
+    if (email == "aksh.d.4002@gmail.com") if (pass == "Aksh1234") return true;
     return false;
   }
 }
