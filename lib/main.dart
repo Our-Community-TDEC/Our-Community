817422a3550117ac5bbf4b11771262841e5ebeba
AkshDesai04 <dev.akshdesai@gmail.com>
2022-08-05 18:38:12 +0530

    Pre change commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,27 @@ import 'package:final_year_project/screens/login_page.dart';
 import 'package:final_year_project/screens/emergency_page.dart';
 import 'package:final_year_project/screens/welcome_page.dart';
 import 'package:final_year_project/screens/noticeboard_page.dart';
-import 'package:final_year_project/screens/complaints_main_page.dart';
 import 'package:final_year_project/screens/other_complaints_page.dart';
-import 'package:final_year_project/screens/BetaTestSection/PiCharts.dart';
 
-void main() => runApp (
-    MaterialApp (
+void main() => runApp(MaterialApp(
       home: BasePage(),
-    )
-);
+    ));
 
 class BasePage extends StatelessWidget {
   @override
-  Widget build (BuildContext context) {
-    return Scaffold (
-      body: Container (
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
         padding: EdgeInsets.all(25),
-        child: Row (
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column (
+            Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ButtonTheme(
                   minWidth: 200,
-                  child:RaisedButton (
+                  child: RaisedButton(
                     // onPressed: () {  },
                     onPressed: () {
                       Navigator.push(
@@ -40,7 +36,7 @@ class BasePage extends StatelessWidget {
                 ),
                 ButtonTheme(
                   minWidth: 200,
-                  child:RaisedButton(
+                  child: RaisedButton(
                     onPressed: () {
                       Navigator.push(
                         context,
@@ -52,11 +48,12 @@ class BasePage extends StatelessWidget {
                 ),
                 ButtonTheme(
                   minWidth: 200,
-                  child:RaisedButton(
+                  child: RaisedButton(
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => EmergencyPage()),
+                        MaterialPageRoute(
+                            builder: (context) => EmergencyPage()),
                       );
                     },
                     child: Text("Emergency Page"),
@@ -64,38 +61,15 @@ class BasePage extends StatelessWidget {
                 ),
                 ButtonTheme(
                   minWidth: 200,
-                  child:RaisedButton(
+                  child: RaisedButton(
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => Complaints_Main_Page()),
-                      );
-                    },
-                    child: Text("Complaints Main Page"),
-                  ),
-                ),
-                ButtonTheme(
-                  minWidth: 200,
-                  child:RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Other_Complaints_Page()),
+                        MaterialPageRoute(
+                            builder: (context) => Other_Complaints_Page()),
                       );
                     },
                     child: Text("Other Complaints Page"),
-                  ),
-                ),
-                ButtonTheme(
-                  minWidth: 200,
-                  child:RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Pie_Chart_Widget()),
-                      );
-                    },
-                    child: Text("Pie Chart (Beta)"),
                   ),
                 ),
               ],
