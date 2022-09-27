524d45c2557ff3e401cef12ef4cb80299d2ddd00
AkshDesai04 <dev.akshdesai@gmail.com>
2022-09-27 13:27:59 +0530

    Temp

--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -1,24 +1,67 @@
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[800],
+      backgroundColor: Colors.black87,
       body: Container(
-        padding: EdgeInsets.all(50),
+        padding: const EdgeInsets.all(15),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Image.asset()
-                Text("Welcome to My Gate"),
-                Text("Let's get started"),
-                RaisedButton(
-                  onPressed: () {  },
-                  child: Text("Get Started"),
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Welcome To Our Community",
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                // const Text(
+                //   "Our Community",
+                //   // textAlign: TextAlign.justify,
+                //   // overflow: TextOverflow.ellipsis,
+                //   style: TextStyle(
+                //     fontSize: 35,
+                //     fontWeight: FontWeight.w600,
+                //     color: Colors.white,
+                //   ),
+                // ),
+                const Text(
+                  "Let's Get Started",
+                  // textAlign: TextAlign.justify,
+                  // overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.lightBlue,
+                    shadowColor: Colors.grey[600],
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    "Get Started",
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
                 ),
               ],
             ),
