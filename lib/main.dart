f2e18d6567bd53f1afe718e02d914c147e96d709
AkshDesai04 <dev.akshdesai@gmail.com>
2022-09-29 15:43:20 +0530

    Temp

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,25 @@ import 'package:final_year_project/screens/complaints_main_page.dart';
 import 'package:final_year_project/screens/other_complaints_page.dart';
 import 'package:final_year_project/screens/BetaTestSection/PiCharts.dart';
 
-void main() => runApp (
-    MaterialApp (
+void main() => runApp(MaterialApp(
       home: BasePage(),
-    )
-);
+    ));
 
 class BasePage extends StatelessWidget {
   @override
-  Widget build (BuildContext context) {
-    return Scaffold (
-      body: Container (
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
         padding: EdgeInsets.all(25),
-        child: Row (
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column (
+            Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ButtonTheme(
                   minWidth: 200,
-                  child:RaisedButton (
+                  child: ElevatedButton(
                     // onPressed: () {  },
                     onPressed: () {
                       Navigator.push(
@@ -40,7 +38,7 @@ class BasePage extends StatelessWidget {
                 ),
                 ButtonTheme(
                   minWidth: 200,
-                  child:RaisedButton(
+                  child: ElevatedButton(
                     onPressed: () {
                       Navigator.push(
                         context,
@@ -52,11 +50,12 @@ class BasePage extends StatelessWidget {
                 ),
                 ButtonTheme(
                   minWidth: 200,
-                  child:RaisedButton(
+                  child: ElevatedButton(
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => EmergencyPage()),
+                        MaterialPageRoute(
+                            builder: (context) => EmergencyPage()),
                       );
                     },
                     child: Text("Emergency Page"),
@@ -64,11 +63,12 @@ class BasePage extends StatelessWidget {
                 ),
                 ButtonTheme(
                   minWidth: 200,
-                  child:RaisedButton(
+                  child: ElevatedButton(
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => Complaints_Main_Page()),
+                        MaterialPageRoute(
+                            builder: (context) => Complaints_Main_Page()),
                       );
                     },
                     child: Text("Complaints Main Page"),
@@ -76,11 +76,12 @@ class BasePage extends StatelessWidget {
                 ),
                 ButtonTheme(
                   minWidth: 200,
-                  child:RaisedButton(
+                  child: ElevatedButton(
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => Other_Complaints_Page()),
+                        MaterialPageRoute(
+                            builder: (context) => Other_Complaints_Page()),
                       );
                     },
                     child: Text("Other Complaints Page"),
@@ -88,11 +89,12 @@ class BasePage extends StatelessWidget {
                 ),
                 ButtonTheme(
                   minWidth: 200,
-                  child:RaisedButton(
+                  child: ElevatedButton(
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => Pie_Chart_Widget()),
+                        MaterialPageRoute(
+                            builder: (context) => Pie_Chart_Widget()),
                       );
                     },
                     child: Text("Pie Chart (Beta)"),
