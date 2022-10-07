d7bbd98ec0a324bffdc56a0bc70acfde00e2dfed
Darshan-Bhuva <bhuvadr46@gmail.com>
2022-10-07 21:27:51 +0530

    Plumber page half done

--- lib/screens/Services/Plumber.dart
+++ lib/screens/Services/Plumber.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-class Plumber extends StatelessWidget {
+import '../../logic/services/plumber/PLumber_logic.dart';
+
+class Plumber extends StatelessWidget with Plumber_logic {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,49 +14,117 @@ class Plumber extends StatelessWidget {
           ]),
           Row(children: [
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  children: [Text("Select Services")],
+                Text(
+                  "Select Services",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  width: 428,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                        onPressed: (() {
+                          Drainage();
+                        }),
+                        style: ElevatedButton.styleFrom(
+                            primary: Color((0xffb4D4E54))),
+                        child: SizedBox(
+                          width: 60,
+                          height: 106,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Image.asset(
+                                'assets/Images/Services/Plumber/image 71.png',
+                                width: 60,
+                                height: 40,
+                              ),
+                              Column(
+                                children: [
+                                  Text("Drainage"),
+                                  Text("System"),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: (() {
+                          Repair();
+                        }),
+                        style: ElevatedButton.styleFrom(
+                            primary: Color((0xffb4D4E54))),
+                        child: SizedBox(
+                          width: 60,
+                          height: 106,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Image.asset(
+                                'assets/Images/Services/Plumber/image 73.png',
+                                width: 60,
+                                height: 40,
+                              ),
+                              Column(
+                                children: [
+                                  Text("Repair"),
+                                  Text("Pipe"),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: (() {
+                          Fitting();
+                        }),
+                        style: ElevatedButton.styleFrom(
+                            primary: Color((0xffb4D4E54))),
+                        child: SizedBox(
+                          width: 60,
+                          height: 106,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Image.asset(
+                                'assets/Images/Services/Plumber/image 76.png',
+                                width: 60,
+                                height: 40,
+                              ),
+                              Column(
+                                children: [
+                                  Text("Fitting"),
+                                  Text("Pipe"),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(
-                      child: Row(children: [
-                        Row(
-                          children: [
-                            //image
-                          ],
-                        ),
-                        Row(
-                          children: [Text("Drainage system")],
-                        ),
-                      ]),
-                    ),
-                    Row(children: [
-                      Row(
-                        children: [
-                          //image
-                        ],
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                      child: Text(
+                        "Data",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w600),
                       ),
-                      Row(
-                        children: [Text("Repair pipe")],
-                      ),
-                    ]),
-                    Container(
-                      child: Row(children: [
-                        Row(
-                          children: [
-                            //image
-                          ],
-                        ),
-                        Row(
-                          children: [Text("Fitting Pipe")],
-                        ),
-                      ]),
-                    ),
+                    )
                   ],
-                )
+                ),
               ],
             ),
             // Next part
