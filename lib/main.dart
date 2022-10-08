c4fb1ef786f53e4ca348e4334ad8ad2faaf7e1f5
tirthp123 <tirthp6414@gmail.com>
2022-10-08 17:14:43 +0530

    Repairs -AD

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:final_year_project/screens/login_page.dart';
-import 'package:final_year_project/screens/emergency_page.dart';
-import 'package:final_year_project/screens/welcome_page.dart';
-import 'package:final_year_project/screens/noticeboard_page.dart';
-import 'package:final_year_project/screens/complaints_main_page.dart';
-import 'package:final_year_project/screens/other_complaints_page.dart';
-import 'package:final_year_project/screens/BetaTestSection/PiCharts.dart';
 
 void main() => runApp (
     MaterialApp (
@@ -27,19 +21,6 @@ class BasePage extends StatelessWidget {
               children: [
                 ButtonTheme(
                   minWidth: 200,
-                  child:RaisedButton (
-                    // onPressed: () {  },
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => WelcomePage()),
-                      );
-                    },
-                    child: Text("Welcome Page"),
-                  ),
-                ),
-                ButtonTheme(
-                  minWidth: 200,
                   child:RaisedButton(
                     onPressed: () {
                       Navigator.push(
@@ -48,54 +29,6 @@ class BasePage extends StatelessWidget {
                       );
                     },
                     child: Text("LogIn Page"),
-                  ),
-                ),
-                ButtonTheme(
-                  minWidth: 200,
-                  child:RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => EmergencyPage()),
-                      );
-                    },
-                    child: Text("Emergency Page"),
-                  ),
-                ),
-                ButtonTheme(
-                  minWidth: 200,
-                  child:RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Complaints_Main_Page()),
-                      );
-                    },
-                    child: Text("Complaints Main Page"),
-                  ),
-                ),
-                ButtonTheme(
-                  minWidth: 200,
-                  child:RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Other_Complaints_Page()),
-                      );
-                    },
-                    child: Text("Other Complaints Page"),
-                  ),
-                ),
-                ButtonTheme(
-                  minWidth: 200,
-                  child:RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Pie_Chart_Widget()),
-                      );
-                    },
-                    child: Text("Pie Chart (Beta)"),
                   ),
                 ),
               ],
--- lib/themes/theme_con.dart
+++
@@ -1,8 +0,0 @@
-  import 'package:flutter/material.dart';
-
-ThemeData light_theme = ThemeData(
-brightness: Brightness.light
-);
-ThemeData dark_theme = ThemeData(
-brightness: Brightness.dark
-);
