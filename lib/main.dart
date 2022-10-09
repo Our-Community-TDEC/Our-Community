ea0af66808d7400ced7fa2d0d509740de2c8d239
AkshDesai04 <dev.akshdesai@gmail.com>
2022-10-09 12:51:28 +0530

    Created HomePage Bra

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:final_year_project/screens/complain_page.dart';
 import 'package:final_year_project/screens/emergency_page.dart';
 import 'package:flutter/material.dart';
 import 'package:final_year_project/screens/welcome_page.dart';
-import 'package:final_year_project/screens/login_page.dart';
+import 'package:final_year_project/screens/home_page.dart';
 
 void main() => runApp(MaterialApp(
       home: BasePage(),
@@ -31,78 +31,10 @@ class BasePage extends StatelessWidget {
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => LogIn()),
+                          MaterialPageRoute(builder: (context) => HomePage()),
                         );
                       },
-                      child: Text("LogIn Page"),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: SizedBox(
-                    width: 200,
-                    height: 30,
-                    child: ElevatedButton(
-                      // onPressed: () {  },
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => WelcomePage()),
-                        );
-                      },
-                      child: Text("Welcome Page"),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: SizedBox(
-                    width: 200,
-                    height: 30,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => EmergencyPage()),
-                        );
-                      },
-                      child: Text("Emergency Page"),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: SizedBox(
-                    width: 200,
-                    height: 30,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ComplainPage()),
-                        );
-                      },
-                      child: Text("Complains Page"),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: SizedBox(
-                    width: 200,
-                    height: 30,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Plumber()),
-                        );
-                      },
-                      child: Text("Services(Plumber) Page"),
+                      child: Text("Home Page"),
                     ),
                   ),
                 ),
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../logic/HomePage_logic.dart';
+
+class HomePage extends StatelessWidget with HomePage_Logic {
+  Color gradient_top = Color(0xFF2E2F36);
+  Color gradient_bot = Color(0xE02E2F36);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
