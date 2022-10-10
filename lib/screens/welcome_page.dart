ce5673f65ac709c3c0eda43bf27dd8ec11bfda58
AkshDesai04 <dev.akshdesai@gmail.com>
2022-10-10 15:03:20 +0530

    Made image size dynamic

--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -1,57 +1,60 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    const double pad = 15;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    double hw_min = min(height, width) - pad;
     return Scaffold(
       backgroundColor: Colors.black87,
       body: Container(
-        padding: const EdgeInsets.all(15),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Image.asset('assets/Images/Welcome/WelcomeImage.png'),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Welcome To Our Community",
-                    style: TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                    ),
-                  ),
+        padding: const EdgeInsets.all(pad),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/Images/Welcome/WelcomeImage.png',
+              width: hw_min,
+            ),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Welcome To Our Community",
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
                 ),
-                const Text(
-                  "Let's Get Started",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
-                  ),
+              ),
+            ),
+            const Text(
+              "Let's Get Started",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shadowColor: Colors.grey[600],
+                elevation: 20,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shadowColor: Colors.grey[600],
-                    elevation: 20,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    "Get Started",
-                    style: TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                "Get Started",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.w500,
                 ),
-              ],
+              ),
             ),
           ],
         ),
