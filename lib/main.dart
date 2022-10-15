9a9cc23e269c2fae8668becc6eebda1f944b1be9
Darshan-Bhuva <bhuvadr46@gmail.com>
2022-10-15 12:32:52 +0530

    Added Continue with google

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:final_year_project/provider/googlesignin.dart';
 import 'package:final_year_project/screens/Services/Plumber.dart';
 import 'package:flutter/material.dart';
 import 'package:final_year_project/screens/complain_page.dart';
@@ -6,120 +5,121 @@ import 'package:final_year_project/screens/emergency_page.dart';
 import 'package:flutter/material.dart';
 import 'package:final_year_project/screens/welcome_page.dart';
 import 'package:final_year_project/screens/login_page.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:final_year_project/provider/googlesignin.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(
-    BasePage()
-  );
-}
+void main() => runApp(
+
+  MultiProvider(
+    providers: [
+      ChangeNotifierProvider<GoogleSignInProviderss>(create: (context)=>GoogleSignInProviderss())
+    ],
+    child: MaterialApp(
+        home: BasePage(),
+      ),
+  ));
 
 class BasePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => ChangeNotifierProvider(
-        create: (context) => GoogleSignInProvider(),
-        child: MaterialApp(
-          home: Container(
-            padding: EdgeInsets.all(25),
-            child: Row(
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(25),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: SizedBox(
-                        width: 200,
-                        height: 30,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => LogIn()),
-                            );
-                          },
-                          child: Text("LogIn Page"),
-                        ),
-                      ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: SizedBox(
+                    width: 200,
+                    height: 30,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LogIn()),
+                        );
+                      },
+                      child: Text("LogIn Page"),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: SizedBox(
-                        width: 200,
-                        height: 30,
-                        child: ElevatedButton(
-                          // onPressed: () {  },
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => WelcomePage()),
-                            );
-                          },
-                          child: Text("Welcome Page"),
-                        ),
-                      ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: SizedBox(
+                    width: 200,
+                    height: 30,
+                    child: ElevatedButton(
+                      // onPressed: () {  },
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => WelcomePage()),
+                        );
+                      },
+                      child: Text("Welcome Page"),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: SizedBox(
-                        width: 200,
-                        height: 30,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => EmergencyPage()),
-                            );
-                          },
-                          child: Text("Emergency Page"),
-                        ),
-                      ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: SizedBox(
+                    width: 200,
+                    height: 30,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => EmergencyPage()),
+                        );
+                      },
+                      child: Text("Emergency Page"),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: SizedBox(
-                        width: 200,
-                        height: 30,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ComplainPage()),
-                            );
-                          },
-                          child: Text("Complains Page"),
-                        ),
-                      ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: SizedBox(
+                    width: 200,
+                    height: 30,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ComplainPage()),
+                        );
+                      },
+                      child: Text("Complains Page"),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: SizedBox(
-                        width: 200,
-                        height: 30,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Plumber()),
-                            );
-                          },
-                          child: Text("Services(Plumber) Page"),
-                        ),
-                      ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: SizedBox(
+                    width: 200,
+                    height: 30,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Plumber()),
+                        );
+                      },
+                      child: Text("Services(Plumber) Page"),
                     ),
-                  ],
+                  ),
                 ),
               ],
             ),
-          ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -2,7 +2,9 @@
 
 import 'dart:ffi';
 
+import 'package:final_year_project/provider/googlesignin.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../logic/login_logic.dart';
 
 class LogIn extends StatelessWidget with Login_Logic {
@@ -14,112 +16,118 @@ class LogIn extends StatelessWidget with Login_Logic {
     TextEditingController emailController = new TextEditingController();
     TextEditingController passController = new TextEditingController();
 
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Icon(Icons.backspace_outlined),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [gradient_top, gradient_bot],
-        )),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Container(
-            padding: EdgeInsets.all(50),
-            margin: EdgeInsets.all(25),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Image(
-                  image: AssetImage('assets/images/Trees.png'),
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [gradient_top, gradient_bot],
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          padding: EdgeInsets.all(50),
+          margin: EdgeInsets.all(25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                  width: 200,
+                  child: Image(
+                    image: AssetImage('assets/Images/Welcome/WelcomeImage.png'),
+                  )),
+              Text("Username"),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey,
+                  border: OutlineInputBorder(),
+                  labelText: 'example@gmail.com',
+                  hintText: 'Enter E-Mail ID',
                 ),
-                Text("Username"),
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey,
-                    border: OutlineInputBorder(),
-                    labelText: 'example@gmail.com',
-                    hintText: 'Enter E-Mail ID',
+              ),
+              Text("password"),
+              TextField(
+                obscureText: true,
+                controller: passController,
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.key),
+                  filled: true,
+                  fillColor: Colors.grey,
+                  border: OutlineInputBorder(),
+                  labelText: '..........',
+                  hintText: 'Enter Password',
+                ),
+                enableSuggestions: false,
+                autocorrect: false,
+                obscuringCharacter: "●",
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  final prov =
+                      Provider.of<GoogleSignInProviderss>(context, listen: false);
+                  prov.googleLogIn();
+                },
+                child: Text(
+                  "Sign in",
+                ),
+                // shape: RoundedRectangleBorder(
+                //   borderRadius: new BorderRadius.circular(45),
+                // ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  final prov =
+                      Provider.of<GoogleSignInProviderss>(context, listen: false);
+                  prov.googleSignOut();
+                },
+                child: Text(
+                  "Sign out",
+                ),
+                // shape: RoundedRectangleBorder(
+                //   borderRadius: new BorderRadius.circular(45),
+                // ),
+              ),
+              Text(
+                "-----------< Or continue with >-----------",
+                style: TextStyle(color: Colors.white),
+              ),
+              Container(
+                width: 400,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  border: Border.all(
+                    width: 2.0,
+                    color: Colors.blue,
+                    style: BorderStyle.solid,
                   ),
                 ),
-                Text("password"),
-                TextField(
-                  obscureText: true,
-                  controller: passController,
-                  decoration: InputDecoration(
-                    suffixIcon: Icon(Icons.key),
-                    filled: true,
-                    fillColor: Colors.grey,
-                    border: OutlineInputBorder(),
-                    labelText: '..........',
-                    hintText: 'Enter Password',
-                  ),
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  obscuringCharacter: "●",
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    signInButtonPressed(
-                        emailController.text, passController.text);
-                  },
-                  child: Text(
-                    "Sign in",
-                  ),
-                  // shape: RoundedRectangleBorder(
-                  //   borderRadius: new BorderRadius.circular(45),
-                  // ),
-                ),
-                Text(
-                  "-----------< Or continue with >-----------",
-                  style: TextStyle(color: Colors.white),
-                ),
-                Container(
-                  width: 400,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    border: Border.all(
-                      width: 2.0,
-                      color: Colors.blue,
-                      style: BorderStyle.solid,
-                    ),
-                  ),
-                  child: SizedBox(
-                    width: 600,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        ElevatedButton(
-                            onPressed: () {
-                              google_button_clicked();
-                            },
-                            child: Text("Google")),
-                        Icon(
-                          Icons.apple,
-                          color: Colors.black,
-                          size: 35,
-                        ),
-                        Icon(
-                          Icons.facebook,
-                          color: Colors.blue,
-                          size: 30,
-                        ),
-                      ],
-                    ),
+                child: SizedBox(
+                  width: 600,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Image.asset(
+                      //   'assets/images/image 3.png',
+                      //   width: 30,
+                      // ),
+                      Icon(
+                        Icons.apple,
+                        color: Colors.black,
+                        size: 35,
+                      ),
+                      Icon(
+                        Icons.facebook,
+                        color: Colors.blue,
+                        size: 30,
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
