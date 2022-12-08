0e29ecf158f3478961693c6f28c157e61ad65f4f
AkshDesai04 <dev.akshdesai@gmail.com>
2022-12-08 19:18:33 +0530

    Updated Script

--- project_script.bat
+++ project_script.bat
@@ -1,4 +1,9 @@
 mkdir C:\Our-Community\
+mkdir C:\src\dart\
+
+cd C:\src\
+git clone https://github.com/flutter/flutter.git -b stable
+
 cd C:\Our-Community\
-git clone https://github.com/Final-Year-Project-TDEC/Final-Year-Project.git
+git clone https://github.com/Our-Community-TDEC/Our-Community.git
 cd Final-Year-Project
