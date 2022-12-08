8176f737502512bf50e958ad52398710041542c3
AkshDesai04 <dev.akshdesai@gmail.com>
2022-12-08 23:36:41 +0530

    Corrected import statment in emergency_logic.dart

--- lib/logic/emergency_logic.dart
+++ lib/logic/emergency_logic.dart
@@ -1,5 +1,5 @@
 import 'package:our_community/main.dart';
-import 'package:our_community/lib/logic/Home_page.dart';
+import 'package:our_community/logic/HomePage_logic.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class Emergency_Logic {
