f789e43221939168c5176d6eaff0a38976d9bee5
Darshan-Bhuva <bhuvadr46@gmail.com>
2022-12-08 15:02:24 +0530

    Revert "work on theme"
    This reverts commit e61f10424c8a40d2e18297c1b4c3acdc6ffc7750.

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,27 @@ Future<void> main() async {
 class BasePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return NeumorphicApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Neumorphic App',
+      themeMode: ThemeMode.light,
+      theme: NeumorphicThemeData(
+        baseColor: Color(0xFFFFFFFF),
+        lightSource: LightSource.topLeft,
+        depth: 10,
+      ),
+      darkTheme: NeumorphicThemeData(
+        baseColor: Color(0xFF3E3E3E),
+        lightSource: LightSource.topLeft,
+        depth: 6,
+      ),
+      home: BasePage1(),
+    );
+  }
+}
+class BasePage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body:
       StreamBuilder<User?>(
