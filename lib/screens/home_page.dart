5cddf7261f9144080b690ccfd8f319b75bd59a6d
AkshDesai04 <dev.akshdesai@gmail.com>
2022-12-16 20:42:07 +0530

    Improved design of home_page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -19,9 +19,10 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     double minHW = min(
         MediaQuery.of(context).size.width, MediaQuery.of(context).size.height);
-    double boxL = minHW * 0.45;
+    double boxL = minHW * 0.42;
     final user = FirebaseAuth.instance.currentUser!;
     String email = user.email!;
+    double square_pad = 10;
 
     FirebaseAuth userauthdata = FirebaseAuth.instance;
     FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -33,6 +34,8 @@ class HomePage extends StatelessWidget {
 
     // var userdata = snapshot.data as DocumentSnapshot;
     // String username = userdata["userName"];
+
+    double offset_val = 2.5;
 
     return Scaffold(
       appBar: AppBar(
@@ -147,108 +150,156 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                "Hey, " + email,
-                style: TextStyle(
-                  fontSize: minHW * 0.05,
-                  fontWeight: FontWeight.bold,
+      body: Container(
+        decoration: BoxDecoration(
+          // color: Colors.black87,
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(217, 46, 47, 54),
+              Color.fromARGB(255, 13, 14, 16),
+            ]
+          )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Hey, " + email,
+                  style: TextStyle(
+                    fontSize: minHW * 0.05,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "Welcome to your community",
+              style: TextStyle(
+                fontSize: minHW * 0.07,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          height: boxL,
+                          width: boxL,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(),
+                            ),
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Notice_Board_Page()),
+                                  );
+                                },
+                                child: Text("NoticeBoard")),
+                          ),
+                        ),
+                    Padding(padding: EdgeInsets.all(square_pad)),
+                        SizedBox(
+                            height: boxL,
+                            width: boxL,
+                            child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(),
+                            ),
+                              child: ElevatedButton(
+                                  onPressed: () {}, child: Text("Events")),
+                            )),
+                      ],
+                    ),
+                    Padding(padding: EdgeInsets.all(square_pad)),
+                    Row(
+                      children: [
+                        SizedBox(
+                            height: boxL,
+                            width: boxL,
+                            child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(),
+                            ),
+                              child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => ComplainPage()),
+                                    );
+                                  },
+                                  child: Text("Complains")),
+                            )),
+                    Padding(padding: EdgeInsets.all(square_pad)),
+                        SizedBox(
+                          height: boxL,
+                          width: boxL,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(),
+                            ),
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => show_suggestion()),
+                                  );
+                                },
+                                child: Text("Suggestions")),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-          Text(
-            "Welcome to your community",
-            style: TextStyle(
-              fontSize: minHW * 0.07,
-              fontWeight: FontWeight.bold,
             ),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        height: boxL,
-                        width: boxL,
-                        child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => Notice_Board_Page()),
-                              );
-                            },
-                            child: Text("Notice Board")),
-                      ),
-                      SizedBox(
-                          height: boxL,
-                          width: boxL,
-                          child: ElevatedButton(
-                              onPressed: () {}, child: Text("Events"))),
-                    ],
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: minHW * 0.15,
+              child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(),
+                            ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EmergencyPage()),
+                    );
+                  },
+                  child: Text("EMERGENCY"),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
                   ),
-                  Row(
-                    children: [
-                      SizedBox(
-                          height: boxL,
-                          width: boxL,
-                          child: ElevatedButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => ComplainPage()),
-                                );
-                              },
-                              child: Text("Complains"))),
-                      SizedBox(
-                        height: boxL,
-                        width: boxL,
-                        child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => show_suggestion()),
-                              );
-                            },
-                            child: Text("Suggestions")),
-                      ),
-                    ],
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.8,
-            height: minHW * 0.075,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EmergencyPage()),
-                );
-              },
-              child: Text("EMERGENCY"),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.red),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
