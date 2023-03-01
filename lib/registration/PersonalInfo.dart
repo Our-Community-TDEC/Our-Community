e947eb1d1149f5f78c9546472baeb9221f457a75
AkshDesai04 <dev.akshdesai@gmail.com>
2023-03-01 11:06:08 +0530

    Temp

---
+++ lib/registration/PersonalInfo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PersonalInfo extends StatelessWidget {
+  const PersonalInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Container(
+
+      )
+    );
+  }
+}
